65ec3df9fc9c321a9e82d70afeb48d977cd61faa
1907FB <994876761@qq.com>
2024-12-10 20:18:58 +0800

    first commit

---
+++ src/datatype/HETensor.h
@@ -0,0 +1,4 @@
+// HETensor should support operations in torch, numpy
+class HETensor{
+    
+}
---
+++ src/layer/Linear.h
@@ -0,0 +1,7 @@
+#include <seal/seal.h>
+
+class Linear{
+    int in_features;
+    int out_features;
+
+}
