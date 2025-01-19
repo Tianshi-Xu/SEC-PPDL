55220ff1abc14384daea6b2efd63d76a30ab3c28
shtechyangchao <yangchao@shanghaitech.edu.cn>
2025-01-19 07:48:43 +0000

    add main

---
+++ src/build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/IOTest/SEC-PPDL/src/HE/NetIO.cpp" "/root/IOTest/SEC-PPDL/src/build/CMakeFiles/test.dir/HE/NetIO.cpp.o"
+  "/root/IOTest/SEC-PPDL/src/main.cpp" "/root/IOTest/SEC-PPDL/src/build/CMakeFiles/test.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../datatype"
+  "../layer/linear"
+  "../layer"
+  "../HE"
+  "/root/IOTest/seal/SEAL/native/src"
+  "/root/IOTest/seal/SEAL/build/native/src"
+  "/root/IOTest/seal/SEAL/build/thirdparty/msgsl-src/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
