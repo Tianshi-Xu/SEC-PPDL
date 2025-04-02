04ec0a40f99ee295eba1b16a3f614b02bd184108
Tianshi-Xu <994876761@qq.com>
2025-04-02 15:05:47 +0800

    .

--- Test/src/TestMatmul.cpp
+++ Test/src/TestMatmul.cpp
@@ -1,4 +1,4 @@
-#include <LinearLayer/Matmul.h>
+#include <LinearLayer/Linear.h>
 #include <Model/ResNet.h>
 #include <fstream>
 #include <iostream>
@@ -21,13 +21,15 @@ int main(int argc, char **argv){
     HE::HEEvaluator HE(netio, party, 8192, 60, Datatype::HOST);
     HE.GenerateNewKey();
     
-    uint64_t d0 = 16; uint64_t d1 = 61; uint64_t d2 = 15;
+    uint64_t d0 = 8; uint64_t d1 = 8; uint64_t d2 = 8;
     Tensor<uint64_t> input({d0, d1}); 
     Tensor<uint64_t> weight({d1, d2});
     Tensor<uint64_t> bias({d0, d2});
-    for(uint32_t i = 0; i < d0; i++){
-        for(uint32_t j = 0; j < d1; j++){
-            input({i, j}) = 1;
+    if(party == ALICE){
+        for(uint32_t i = 0; i < d0; i++){
+            for(uint32_t j = 0; j < d1; j++){
+                input({i, j}) = 1;
+            }
         }
     }
     for(uint32_t i = 0; i < d1; i++){
@@ -35,13 +37,18 @@ int main(int argc, char **argv){
             weight({i, j}) = 1;
         }
     }
+    if(party == ALICE){
+        input.print();
+        weight.print();
+    }
+
     cout << "input generated" << endl;
-    // MatmulCtptBolt* matmul1 = new MatmulCtptBolt(d0, weight, weight, &HE);
-    // Tensor<uint64_t> output1 = matmul1->operator()(input);
-    // output1.print();
-    MatmulCtctBumble* matmul2 = new MatmulCtctBumble(&HE);
-    Tensor<uint64_t> output2 = matmul2->operator()(input, weight);
-    output2.print();
+    LinearBolt* matmul1 = new LinearBolt(d0, weight, weight, &HE);
+    Tensor<uint64_t> output1 = matmul1->operator()(input);
+    output1.print(10);
+    // MatmulCtctBumble* matmul2 = new MatmulCtctBumble(&HE);
+    // Tensor<uint64_t> output2 = matmul2->operator()(input, weight);
+    // output2.print();
 
     return 0;
 }
